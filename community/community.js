c38cee3b9d40b9ea9468ce0f7991ff550051891d
Huynh Truong <huynhphuoctruongk17086@gmail.com>
2022-02-07 13:33:59 +0700

    update community

---
+++ community/community.js
@@ -0,0 +1,48 @@
+const community = [
+  {
+    name: 'manifesto',
+    logoImg:
+      'https://cdn.shopify.com/s/files/1/0208/1956/t/46/assets/mega-menu-89051-b0001-pagesimmanifesto-logo-menu-1382612436_320x.jpg?v=1641480955',
+    backgroundImg:
+      'https://i.shgcdn.com/86c8666e-b2e7-4523-ad1a-398949505a3d/-/format/auto/-/preview/3000x3000/-/quality/lighter/',
+  },
+  {
+    name: 'jackie club',
+    logoImg:
+      'https://cdn.shopify.com/s/files/1/0208/1956/t/30/assets/mega-menu-89051-jackie-club-1552575789_320x.jpg?v=1623168971',
+    backgroundImg:
+      'https://i.shgcdn.com/d6bfe4fb-6716-4d7d-aa43-0f99e7852114/-/format/auto/-/preview/3000x3000/-/quality/lighter/',
+  },
+  {
+    name: '#myjackie',
+    logoImg:
+      'https://cdn.shopify.com/s/files/1/0208/1956/t/46/assets/mega-menu-89051-b0001-pagesimmyjackie-logo-menu-446179338_320x.jpg?v=1641480958',
+    backgroundImg:
+      'https://i.shgcdn.com/0aaae475-4dd0-4453-a960-6574ba070930/-/format/auto/-/preview/3000x3000/-/quality/lighter/',
+  },
+  {
+    name: '#art&play',
+    logoImg: '',
+    backgroundImg:
+      'https://i.shgcdn.com/7e806a17-6562-4d14-af1c-d3d39ca77ef2/-/format/auto/-/preview/3000x3000/-/quality/lighter/',
+  },
+  {
+    name: '#thepinkbox',
+    logoImg: '',
+    backgroundImg:
+      'https://i.shgcdn.com/ae803fea-78e3-47df-a51e-fee261aae0a4/-/format/auto/-/preview/3000x3000/-/quality/lighter/',
+  },
+  {
+    name: 'shop our ins',
+    logoImg:
+      'https://cdn.shopify.com/s/files/1/0208/1956/t/46/assets/mega-menu-89051-b0001-pagesimshopourinsta-logo-menu-345233605_320x.jpg?v=1641480966',
+    backgroundImg: 'https://cdn.shopify.com/s/files/1/0208/1956/files/shopInsta.jpg',
+  },
+  {
+    name: 'reviews',
+    logoImg:
+      'https://cdn.shopify.com/s/files/1/0208/1956/t/52/assets/mega-menu-89051-b0001-pagesimreviews-logo-menu-1985432375_320x.jpg?v=1642430838',
+    backgroundImg:
+      'https://cdn.shopify.com/s/files/1/0208/1956/t/52/assets/jackie_smith-reviews-desktop_1300x.png?v=12351189413056986851',
+  },
+]
